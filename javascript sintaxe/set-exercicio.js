bd4423925fd32c4483a595a0f0ceacb3c8259efe
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-10-09 17:12:23 -0300

    set exercicio

---
+++ javascript sintaxe/set-exercicio.js
@@ -0,0 +1,12 @@
+// Atividade: Sets
+// Dado o array [30, 30, 40, 5, 223, 2049, 3034, 5], retorne outro array apenas com valores únicos.
+
+const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+function valoresUnicos(array) {
+  const mySet = new Set(array);
+
+  return [...mySet];
+}
+
+console.log(valoresUnicos(meuArray));
