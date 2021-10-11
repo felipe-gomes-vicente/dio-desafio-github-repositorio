f05793556d39eff87627211027ba69c751d3d7dd
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-10-11 19:12:33 -0300

    exercicio modulo

---
+++ javascript sintaxe/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Felipe', 26));
+console.log(mostraCidade('Felipe', 'São Paulo'));
+console.log(mostraHobby('Felipe', 'Praticar Inglês'));
