c20082b9ffaf5cbc294faf18fe2c607b0fc72bf5
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-10-11 21:57:51 -0300

    exercicio debug error

---
+++ javascript sintaxe/debug-error.js
@@ -0,0 +1,43 @@
+// Atividade: validação de erros por tipo
+// O objetivo é que a função receba um array e retorne ele caso o seu tamanho corresponda ao número enviado como parâmetro na função. Caso contrário, um erro será lançado.
+
+// Crie uma função que recebe um array e um número
+// Realize as seguintes validações
+// Se os parâmetros não forem enviados, lance um erro do tipo ReferenceError
+// Se o array não for do tipo 'object', lance um erro do tipo TypeError
+// Se o número não for do tipo 'number', lance um erro do tipo TypeError
+// Se o tamanho do array for diferente do número enviado como parâmetro, lance um erro do tipo RangeError
+// Utilize a declaração try...catch
+// Filtre as chamadas de catch por cada tipo de erro utilizando o operador instanceof
+
+function validaArray(array, numero) {
+  try {
+    if (!array && !numero) throw new ReferenceError("Envie os parâmetros");
+
+    if (typeof array !== "object")
+      throw new TypeError("Array precisa ser do tipo object");
+
+    if (typeof numero !== "number")
+      throw new TypeError("Num precisa ser do tipo number");
+
+    if (array.length !== numero) throw new RangeError("Tamanho inválido");
+    
+    return array;
+  } 
+  catch (e) {
+    if(e instanceof ReferenceError) {
+      console.log("Este erro é um ReferenceError")
+      console.log(e.message)
+    } else if (e instanceof TypeError) {
+      console.log("Este erro é um TypeError")
+      console.log(e.message)
+    } else if (e instanceof RangeError) {
+      console.log("Este erro é um RangeError")
+      console.log(e.message)
+    } else {
+      console.log("Tipo de erro não esperado:" + e);
+    }
+  }
+}
+
+console.log(validaArray([1, 2, 3, 4, 5], 5))
