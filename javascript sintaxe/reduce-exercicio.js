cff2bb5dccb255c6aae2d753d12b4828e282a03f
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-10-12 22:29:41 -0300

    reduce exercicio

--- javascript sintaxe/reduce-exercicio.js
+++ javascript sintaxe/reduce-exercicio.js
@@ -1,7 +1,5 @@
 // Reduce
 // Some todos os números de um array
-// Crie uma função que recebe uma lista de preços e um número representando o saldo disponível. Calcule qual será o saldo final após subtrair todos os preços da lista enviada.
-
 function somaNumeros(array) {
   return array.reduce(function(prev,current) {
     console.log({ prev })
